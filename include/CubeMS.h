fe11e5be46e7596e77d806ec7ee184b00cfe4e5a
EvelynMakesThings <evelynmakesthings@gmail.com>
2025-06-25 22:33:31 -0500

    Placeholder structures added and basic printout

--- include/CubeMS.h
+++ include/CubeMS.h
@@ -1,5 +1,15 @@
-// As of now, this is just a placeholder. Please ignore.
+#include <netinet/in.h>
+
 #ifndef CUBEMS_H
 #define CUBEMS_H
+
+// Server Info Structure (Recieved from servers and temporarily stored.)
+typedef struct ServerInfo {
+	char ip[INET_ADDRSTRLEN];
+	char desc[128];
+	int players;
+	int maxplayers;
+	char map[64];
+} ServerInfo;
 
 #endif
--- src/main.c
+++ src/main.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
+#include "CubeMS.h"
 
 int main() {
-    printf("Hello, World!\n");
+    ServerInfo test = {"example.com","Example Server Structure",1,20,"default"}; // Placeholder Data
+    printf("        Server Printout\n");
+    printf("--------------------------------\n");
+    printf("Server Address:  %s\n",test.ip);
+    printf("Server Desc:     %s\n",test.desc);
+    printf("Current Players: %d\n",test.players);
+    printf("Max Players:     %d\n",test.maxplayers);
+    printf("Current Map:     %s\n",test.map);
     return 0;
 }
