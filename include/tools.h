c470e505e517480c15b4f7e50af4f60227119918
EvelynMakesThings <evelynmakesthings@gmail.com>
2025-06-30 15:08:11 -0500

    Server now stores and responds with server ips. Server ips are invalidated after an hour.

--- include/tools.h
+++ include/tools.h
@@ -1,8 +1,17 @@
 #include <stddef.h>
+#include "CubeMS.h"
 #ifndef TOOLS_H
 #define TOOLS_H
 
 // Safer string copy, ensure null terminated string.
 void strcpyS(char *__restrict__ __dest, const char *__restrict__ __src, size_t __n);
+// Append to null terminated string of BUFF_SIZE length.
+void strcpyA(char* __dest, const char* __src);
+// Add a server to buffer, should be safe
+void AddServer(char* BUFF, char* Server);
+// Allocate / Renew a server in the server slots.
+int UpdateServer(ip Addresses[],const char* address);
+// Return remaining servers after disabling timed out servers.
+int TickServers(ip Addresses[]);
 
 #endif
